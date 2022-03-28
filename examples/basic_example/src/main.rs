f45e05581ea12d60452f7e35007caae6f9464a69
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2022-03-28 23:42:59 +0200

    Add width and height

--- examples/basic_example/src/main.rs
+++ examples/basic_example/src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+	let viewport = ferrux_viewport::viewport::Viewport::new(640 as u32, 480 as u32);
+	assert_eq!(640, viewport.width());
+	assert_eq!(480, viewport.height());
 }
--- src/viewport.rs
+++ src/viewport.rs
@@ -1,6 +1,7 @@
 use crate::{PixelSize, Position, Coordinate};
 use crate::error::ViewportError;
 
+// temporary
 type Color<'a> = &'a [u8; 4];
 
 pub struct Viewport<S: PixelSize> {
@@ -9,28 +10,68 @@ pub struct Viewport<S: PixelSize> {
 }
 
 impl<S: PixelSize> Viewport<S> {
-	/* Extract to trait Size */
-	fn width(&self) -> S {
+
+	/// Builds a new Viewport to use.
+	/// 
+	/// # Arguments
+	/// * `width`. Width in pixels of the screen, must be an unsigned value.
+	/// * `height`. Height in pixels of the screen, must be an unsigned value.
+	/// 
+	/// # Example
+	/// ```no_run
+	/// let viewport = Viewport::new(640 as u32, 480 as u32);
+	/// ```
+	/// 
+	pub fn new(width: S, height: S) -> Self {
+		// TODO build buffer and store window
+		Viewport { width, height }
+	}
+
+	/// Returns the width of the current window
+	pub fn width(&self) -> S {
 		self.width
 	}
-	fn height(&self) -> S {
+
+	/// Returns the height of the current window
+	pub fn height(&self) -> S {
 		self.height
 	}
-	fn resize(&mut self, width: S, height: S) {}
 
-	/* Extract to trait Render */	
-	fn render(&mut self) -> Result<(), ViewportError> {
+	/// Changes the size of the rendered window
+	/// 
+	/// # Arguments
+	/// * `width`. New width of the window.
+	/// * `height`. New height of the window.
+	/// 
+	pub fn resize(&mut self, width: S, height: S) {
+		self.width = width;
+		self.height = height;
+		// TODO resize buffer
+	}
+
+	/// TODO	
+	pub fn render(&mut self) -> Result<(), ViewportError> {
 		Ok(())
 	}
-	fn clear_frame(&mut self) -> Result<(), ViewportError> {
+
+	//TODO
+	pub fn clear_frame(&mut self) -> Result<(), ViewportError> {
 		Ok(())
 	}
 
-	/* Extract to trait Draw */
-	fn draw_point<P: Coordinate>(&mut self, position: Position<P>, color: Color) {}
-	fn draw_line<P: Coordinate>(&mut self, start: Position<P>, end: Position<P>, color: Color) {}
-	fn draw_triangle<P: Coordinate>(&mut self, point_a: Position<P>, point_b: Position<P>, point_c: Position<P>, color: Color) {}
-	fn fill_triangle<P: Coordinate>(&mut self, point_a: Position<P>, point_b: Position<P>, point_c: Position<P>, color: Color) {}
-	fn reset(&mut self) {}
+	///TODO
+	pub fn draw_point<P: Coordinate>(&mut self, position: Position<P>, color: Color) {}
+	
+	/// TODO
+	pub fn draw_line<P: Coordinate>(&mut self, start: Position<P>, end: Position<P>, color: Color) {}
+	
+	///TODO
+	pub fn draw_triangle<P: Coordinate>(&mut self, point_a: Position<P>, point_b: Position<P>, point_c: Position<P>, color: Color) {}
+	
+	///TODO
+	pub fn fill_triangle<P: Coordinate>(&mut self, point_a: Position<P>, point_b: Position<P>, point_c: Position<P>, color: Color) {}
+	
+	///TODO
+	pub fn reset(&mut self) {}
 
 }
